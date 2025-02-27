7a214263a06acfc9f971a6383efa0f4de1e5d944
Jamkit1012 <137848264+Jamkit1012@users.noreply.github.com>
2025-02-27 15:35:36 +0800

    Update ham_twoborder.f90

--- ham_twoborder.f90
+++ ham_twoborder.f90
@@ -1,4 +1,4 @@
-subroutine ham_ribbon(k, Hamk_ribbon)
+subroutine ham_twoborder(k, Hamk_ribbon)
 
      use para
      implicit none
@@ -10,7 +10,7 @@ subroutine ham_ribbon(k, Hamk_ribbon)
      real(Dp) :: k
 
 ! Hamiltonian of slab system
-     complex(Dp),intent(out) :: Hamk_ribbon(Num_wann*nslab1*nslab2, &
+     complex(Dp),intent(out) :: ham_twoborder(Num_wann*nslab1*nslab2, &
         Num_wann*nslab1*nslab2)
 
 ! the factor 2 is induced by spin
@@ -19,14 +19,14 @@ subroutine ham_ribbon(k, Hamk_ribbon)
      allocate(Hij(-ijmax:ijmax,-ijmax:ijmax,Num_wann,Num_wann))
      call ham_qlayer2qlayerribbon(k,Hij)    
     ! Initialize Hamiltonian
-    Hamk_ribbon = 0.0d0
+    ham_twoborder = 0.0d0
     
     ! Construct the Hamiltonian
     do i1 = 1, nslab1
         do j1 = 1, nslab2
             do i2 = max(1, i1 - ijmax), min(nslab1, i1 + ijmax)
                 do j2 = max(1, j1 - ijmax), min(nslab2, j1 + ijmax) 
-                    Hamk_ribbon(idx(i1, j1):idx(i1, j1) + Num_wann - 1, &
+                    ham_twoborder(idx(i1, j1):idx(i1, j1) + Num_wann - 1, &
                                 idx(i2, j2):idx(i2, j2) + Num_wann - 1) &
                                   = Hij(i2 - i1, j2 - j1, :, :)
                 end do
@@ -45,4 +45,4 @@ contains
         ind = (i - 1) * nslab2 * Num_wann + (j - 1) * Num_wann + 1
     end function idx
     
-end subroutine ham_ribbon
+end subroutine ham_twoborder
